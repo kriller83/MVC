43f3c943b055569a8866eb6313a7e2b40339a8f0
kriller83 <krirag12@student.hh.se>
2016-04-18 14:51:40 +0200

    lagt till nytt projekt plus en controller

---
+++ ASP.NET Labbar/MVCLabb1/Controllers/SupermanController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCLabb1.Controllers
+{
+    public class SupermanController : Controller
+    {
+        // GET: Superman
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ASP.NET Labbar/MVCLabb1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCLabb1.Startup))]
+namespace MVCLabb1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
